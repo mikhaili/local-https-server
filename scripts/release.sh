388c6450a7936836d97be90dc096caa14b69e182
Michael Iline <michael.iline@ironsrc.com>
2020-07-29 15:34:45 +0300

    fix port number

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp './docs/lhs.config.json' './dist'
 zip -r "./release/https-server-$npm_package_version.zip" \
   './dist' './mocks-response' \
-  package.json README.md
+  package.json README.md lhs.config.json
